4b4e5d739b099cabe8788c3ecaf15945150a8fed
StefanvanTilburg <TillyAliasStefan@gmail.com>
2020-09-26 14:19:59 +0200

    Opdracht 3.2 Voetbalscores stap 7 resultaat

--- src/controller/VoetbalScoresLauncher.java
+++ src/controller/VoetbalScoresLauncher.java
@@ -15,44 +15,38 @@ import java.util.Scanner;
 public class VoetbalScoresLauncher {
     public static void main(String[] args) {
 
-        Team homeTeam = new Team("Ajax");
-        Team awayTeam = new Team("PEC");
-        Wedstrijd game = new Wedstrijd(homeTeam, awayTeam, 5, 0);
-        game.printUitslag();
-        game.printWedstrijdPunten() ;
-
-        Team[] teams = new Team[3];
-        teams[0] = new Team ("VVV");
-        teams[1] = new Team ("NAC");
-        teams[2] = new Team ("PEC");
-
-        Wedstrijd[] wedstrijden = new Wedstrijd[6];
-//        wedstrijden[0] = new Wedstrijd(teams[0], teams[1]); //VVV-NAC
-//        wedstrijden[1] = new Wedstrijd(teams[0], teams[2]); //VVV-PEC
-//        wedstrijden[2] = new Wedstrijd(teams[1], teams[2]); //NAC-PEC
-//        wedstrijden[3] = new Wedstrijd(teams[2], teams[0]); //PEC-VVV
-//        wedstrijden[4] = new Wedstrijd(teams[2], teams[1]); //PEC-NAC
-//        wedstrijden[5] = new Wedstrijd(teams[1], teams[0]); //NAC-VVV
-        wedstrijden[0] = new Wedstrijd(teams[0],teams[1]); //VVV-NAC
-        wedstrijden[1] = new Wedstrijd(teams[0],teams[2]); //VVV-PEC
-        wedstrijden[2] = new Wedstrijd(teams[1],teams[2]); //NAC-PEC
-        wedstrijden[3] = new Wedstrijd(teams[1],teams[0]); //NAC-VVV
-        wedstrijden[4] = new Wedstrijd(teams[2],teams[0]); //PEC-VVV
-        wedstrijden[5] = new Wedstrijd(teams[2],teams[1]); //PEC-NAC
-
-        // Tja als je de scores op eenzelfde wijze invoert, maar wedstrijd array anders vult.
-        // Dan is het natuurlijk logisch dat doelsaldo ect anders uitkomt.
-
         Scanner keyboard = new Scanner(System.in);
+        System.out.print("Voor hoeveel teams wordt uitslagen bijgehouden? ");
+        int teamsAantal = keyboard.nextInt();
+
+        Team[] teams = new Team[teamsAantal];
+        for (int team = 0; team < teams.length; team++) {
+            System.out.printf("\nGeef naam voor team %d: ", team + 1);
+            teams[team] = new Team(keyboard.next());
+        }
+
+        Wedstrijd[] wedstrijden = new Wedstrijd[teamsAantal * (teamsAantal - 1)];
+        int wedstrijdIndex = 0;
+        for (int teamThuis = 0; teamThuis < teams.length - 1; teamThuis++) {
+            for (int teamUit = teamThuis + 1; teamUit < teams.length; teamUit++) {
+                wedstrijden[wedstrijdIndex++] = new Wedstrijd(teams[teamThuis],teams[teamUit]);
+            }
+        }
+        for (int teamThuis = teams.length - 1; teamThuis >= 0; teamThuis--) {
+            for (int teamUit = teamThuis - 1; teamUit >= 0; teamUit--) {
+                wedstrijden[wedstrijdIndex++] = new Wedstrijd(teams[teamThuis],teams[teamUit]);
+            }
+        }
+
         for (Wedstrijd wedstrijd : wedstrijden) {
             System.out.printf("\nGeef van de wedstrijd %s-%s de voordoelpunten: ",
-                    wedstrijd.getTeamThuis().getTeamNaam(),
-                    wedstrijd.getTeamUit().getTeamNaam());
+                                    wedstrijd.getTeamThuis().getTeamNaam(),
+                                    wedstrijd.getTeamUit().getTeamNaam());
             wedstrijd.setVoorDoelpunten(keyboard.nextInt());
 
             System.out.printf("\nGeef van de wedstrijd %s-%s de tegendoelpunten: ",
-                    wedstrijd.getTeamThuis().getTeamNaam(),
-                    wedstrijd.getTeamUit().getTeamNaam());
+                                    wedstrijd.getTeamThuis().getTeamNaam(),
+                                    wedstrijd.getTeamUit().getTeamNaam());
             wedstrijd.setTegenDoelpunten(keyboard.nextInt());
 
             wedstrijd.setTotaalDoelpuntenTeams();
